bb63b8ca27dff41d8d0037f027af5150991f1fc2
EC2 Default User <ec2-user@ip-172-31-29-114.ec2.internal>
2022-03-18 05:14:50 +0000

    if condition

---
+++ colour.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo -e "\e[31mNarendra\e[0m"
+echo "hai"
+if $a=1 ; then
+echo -e "narendra \n indra"
+echo -e "\e[32mnaren\e[0m \t \e[34msowj\e[0m"
+else
+echo "if condition working"
+fi
+a=10
+b=15
+
